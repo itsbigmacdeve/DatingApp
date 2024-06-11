d11a3c1e9e63cd0988cbb457fcc466bb4d7b6f40
Víctor Ponce <60446086+victorponce518755@users.noreply.github.com>
2024-06-11 14:27:19 -0600

    termino de la seccion 4

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,26 @@
+using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddCors();
+            // se agregan los servicios de token, la interfaz y la clase
+            services.AddScoped<ITokenService, TokenService>();
+
+            return services;
+            
+        }
+        
+    }
+}
