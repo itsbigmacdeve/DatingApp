dbbf04ac3cdf37bb42de106caae71daec19ffe26
Víctor Ponce <60446086+victorponce518755@users.noreply.github.com>
2024-06-12 16:35:51 -0600

    feat: Add ngx-toastr module for displaying error messages listo hasta el 67

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -11,7 +12,10 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter(); // Se crea un evento para cancelar el registro
   model: any = {};
 
-  constructor(private accountService: AccountService) {}
+  constructor(
+    private accountService: AccountService,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit(): void {}
 
@@ -21,6 +25,7 @@ export class RegisterComponent implements OnInit {
         this.cancel();
       },
       error: (error) => {
+        this.toastr.error(error.error);
         console.log(error);
       },
     });
