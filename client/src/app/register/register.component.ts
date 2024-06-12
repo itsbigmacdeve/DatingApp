00a0029d054696995c3537bf04f05e30e7d55d84
Víctor Ponce <60446086+victorponce518755@users.noreply.github.com>
2024-06-12 10:26:18 -0600

    Casi terminado el modulo 5

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-register',
@@ -6,18 +7,26 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-  @Input() usersFromHomeComponent: any;
+  //@Input() usersFromHomeComponent: any;
+  @Output() cancelRegister = new EventEmitter(); // Se crea un evento para cancelar el registro
   model: any = {};
 
-  constructor() {}
+  constructor(private accountService: AccountService) {}
 
   ngOnInit(): void {}
 
   register() {
-    console.log(this.model);
+    this.accountService.register(this.model).subscribe({
+      next: () => {
+        this.cancel();
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
   }
 
   cancel() {
-    console.log('cancelled');
+    this.cancelRegister.emit(false); // Despues de crea el metodo para cancelar la emsiion del evento
   }
 }
