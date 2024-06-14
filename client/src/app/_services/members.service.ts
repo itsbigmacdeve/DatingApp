0ae21c1995203947738374ada949e0ffe3d2c5b9
victorponce518755 <60446086+victorponce518755@users.noreply.github.com>
2024-06-14 17:28:27 -0600

    Termino de seccion 10

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -2,27 +2,39 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/members';
+import { map, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MembersService {
   baseUrl = environment.apiUrl;
+  members : Member[] = [];
 
   constructor(private http: HttpClient) { }
 
   //Este metodo obtiene todos los miembros
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'userscontrollers');
+    if (this.members.length > 0) return of(this.members);
+    return this.http.get<Member[]>(this.baseUrl + 'userscontrollers').pipe(
+      map(members => {this.members = members; return members;}));
   }
+
 
   //Este metodo obtiene un miembro por username
   getMember(username: string) {
+    const member = this.members.find(x => x.userName === username);
+    if (member) return of(member);
     return this.http.get<Member>(this.baseUrl + 'userscontrollers/' + username);
   }
 
   updareMember(member: Member) {
-    return this.http.put(this.baseUrl + 'userscontrollers', member);
+    return this.http.put(this.baseUrl + 'userscontrollers', member).pipe(
+      map(() => {
+        const index = this.members.indexOf(member);
+        this.members[index] = {...this.members[index], ...member};
+      })
+    );
   }
 
 
