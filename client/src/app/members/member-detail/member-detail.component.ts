3aa1a25a59803d8e34020ade70bed19331c392a4
victorponce518755 <60446086+victorponce518755@users.noreply.github.com>
2024-06-14 13:00:58 -0600

    listo hasta seccion  9, en la parte 111, pero sin acabar

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,15 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { GalleryItem, GalleryModule } from 'ng-gallery';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 import { Member } from 'src/app/_models/members';
 import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
   selector: 'app-member-detail',
+  standalone: true,
   templateUrl: './member-detail.component.html',
-  styleUrls: ['./member-detail.component.css']
+  styleUrls: ['./member-detail.component.css'],
+  imports: [CommonModule, TabsModule, GalleryModule]
 })
 export class MemberDetailComponent implements OnInit{
   member : Member | undefined;
+  images: GalleryItem[] = [];
+
   constructor(private memberService: MembersService, private route: ActivatedRoute ) { }
 
   ngOnInit(): void {
@@ -23,5 +30,7 @@ export class MemberDetailComponent implements OnInit{
       next: member => this.member = member
     })
   }
+
+  
 
 }
