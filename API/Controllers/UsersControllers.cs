b64529f61f52dbc888cbe19daa6f75b24ec4c6e9
victorponce518755 <60446086+victorponce518755@users.noreply.github.com>
2024-06-15 11:28:52 -0600

    ya funciono lo que fallaba

--- API/Controllers/UsersControllers.cs
+++ API/Controllers/UsersControllers.cs
@@ -90,9 +90,9 @@ public class UsersControllers : BaseApiController
 
         user.Photos.Add(photo);
 
-        if (await _userRepository.SaveAllAsync()) //return _mapper.Map<PhotoDto>(photo);
+        if (await _userRepository.SaveAllAsync()) //return _mapper.Map<PhotoDto>(photo);  
         {
-            return CreatedAtRoute("GetUser", new {username = user.UserName}, _mapper.Map<PhotoDto>(photo)); 
+            return CreatedAtAction("GetUser", new {username = user.UserName}, _mapper.Map<PhotoDto>(photo)); 
         }
 
         return BadRequest("Problem adding photo");
