f94fac0bee2ea949c0e51a6d98c2b000602e2bb3
Víctor Ponce <60446086+victorponce518755@users.noreply.github.com>
2024-06-17 13:02:02 -0600

    validando el segundo password, mas aun falta validar el primero

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,7 +1,14 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
-import { Form, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  Form,
+  FormControl,
+  FormGroup,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -12,7 +19,7 @@ export class RegisterComponent implements OnInit {
   //@Input() usersFromHomeComponent: any;
   @Output() cancelRegister = new EventEmitter(); // Se crea un evento para cancelar el registro
   model: any = {};
-  registerForm: FormGroup = new FormGroup({}); 
+  registerForm: FormGroup = new FormGroup({});
 
   constructor(
     private accountService: AccountService,
@@ -23,12 +30,28 @@ export class RegisterComponent implements OnInit {
     this.initializeForm();
   }
 
-  initializeForm() {//TODO ESTE METODO CAMBIO
+  initializeForm() {
+    //TODO ESTE METODO CAMBIO
     this.registerForm = new FormGroup({
-      username: new FormControl("Hello", Validators.required ),
-      password: new FormControl("" , [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
-      confirmPassword: new FormControl(),
+      username: new FormControl('Hello', Validators.required),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(8),
+      ]),
+      confirmPassword: new FormControl('', [
+        Validators.required,
+        this.matchValues('password'),
+      ]),
     });
+  }
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      return control.value === control.parent?.get(matchTo)?.value
+        ? null
+        : { notMatchin: true };
+    };
   }
 
   register() {
