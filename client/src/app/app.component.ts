313f4de7dec4fe3fcf92f7a389790aa0a849f7bf
victorponce518755 <60446086+victorponce518755@users.noreply.github.com>
2024-06-18 10:00:11 -0600

    chore: Update .gitignore and environment files

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,10 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from './_services/account.service';
 import { User } from './_models/user';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
   title = 'Dating App';
