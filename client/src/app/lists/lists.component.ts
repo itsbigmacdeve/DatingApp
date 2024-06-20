bebe738aca64eea303d20f046be0656b0f75c6cd
victor11s <60446086+victorponce518755@users.noreply.github.com>
2024-06-20 17:03:15 -0600

    creo que el bug si viene, de cuando se envia el argumento

--- client/src/app/lists/lists.component.ts
+++ client/src/app/lists/lists.component.ts
@@ -1,28 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { Member } from '../_models/members';
 import { MembersService } from '../_services/members.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-lists',
   templateUrl: './lists.component.html',
-  styleUrls: ['./lists.component.css']
+  styleUrls: ['./lists.component.css'],
 })
 export class ListsComponent implements OnInit {
-  members : Member[]  |  undefined;
+  members: Member[] | undefined;
   predicate = 'liked';
 
-  constructor(private memberService: MembersService) { }
+  constructor(private memberService: MembersService) {}
 
   ngOnInit(): void {
     this.loadLikes();
   }
 
-  loadLikes(){
+  loadLikes() {
     this.memberService.getLikes(this.predicate).subscribe({
-      next: response  => {
-        this.members = response 
-      }
+      next: (response) => {
+        console.log(this.predicate);
+        this.members = response;
+      },
     });
   }
-
 }
