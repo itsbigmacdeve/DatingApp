610a3f946c59afa9f0d4dc353bbfd4fa7fa21b75
victor11s <60446086+victorponce518755@users.noreply.github.com>
2024-06-21 14:43:28 -0600

    listo hasta video 449

--- API/Controllers/MessagesControllers.cs
+++ API/Controllers/MessagesControllers.cs
@@ -1,6 +1,7 @@
 using API.DTOs;
 using API.Entities;
 using API.Extensions;
+using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
@@ -49,6 +50,17 @@ namespace API.Controllers
             return BadRequest("Failed to send message");
 
 
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MessageDto>>> GetMessagesForUser([FromQuery] MessageParams messageParams){
+            messageParams.Username = User.GetUsername();
+
+            var messages = await _messageRepository.GetMessagesForUser(messageParams);
+
+            Response.AddPaginationHeader(new PaginationHeader(messages.CurrentPage, messages.PageSize, messages.TotalCount, messages.TotalPages));
+
+            return messages;
         }
     }
 }
